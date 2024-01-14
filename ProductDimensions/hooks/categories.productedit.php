d513f2237505cce9e70c378ac6de5da754394802
hybiepoo <hybiepoo@hotmail.com>
2024-01-14 18:29:20 +1100

    Delete ProductDimensions directory
    This is no longer needed

--- ProductDimensions/hooks/categories.productedit.php
+++
@@ -1,5 +0,0 @@
-<?php
-if ($ProductDimensions = \common\helpers\Acl::checkExtensionAllowed('ProductDimensions', 'allowed')) {
-	$ProductDimensions::saveProductField((int) $pInfo->product_id);
-	//$CustomersRank::saveCustomerField((int) $cInfo->customers_id);
-}
--- ProductDimensions/hooks/categories.productedit.size-bottom.tpl
+++
@@ -1,3 +0,0 @@
-{if $ext = \common\helpers\Acl::checkExtensionAllowed('ProductDimensions', 'allowed')}
-    {$ext::renderDimensionsField($pInfo->products_id)}
-{/if}
